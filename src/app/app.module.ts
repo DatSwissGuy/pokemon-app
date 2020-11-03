90661c2d37bbc0d7b506823552bda5b5945c121d
DatSwissGuy <kxosz9@outlook.com>
2020-11-03 22:29:49 +0100

    Clean up code and dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
-import { StoreModule } from '@ngrx/store';
-import { metaReducers, reducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -25,12 +19,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserModule.withServerTransition({appId: 'serverApp'}),
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers
-    }),
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
-    EffectsModule.forRoot([]),
-    StoreRouterConnectingModule.forRoot(),
     BrowserAnimationsModule,
     MatCardModule,
     MatIconModule,
