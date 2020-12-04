dc08701ce413bbf54283473ad17bd5fb093c1cc6
DatSwissGuy <kxosz9@outlook.com>
2020-12-04 17:29:59 +0100

    Add games for generation.

--- src/app/pages/generations/generations.component.ts
+++ src/app/pages/generations/generations.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from '../../services/poke-api.service';
+import { GenerationCollection } from '../../model/generation-collection';
+import * as games4Generation from '../../data/games4generation.json';
 
 @Component({
   selector: 'app-generations',
@@ -8,7 +10,7 @@ import { PokeApiService } from '../../services/poke-api.service';
 })
 export class GenerationsComponent implements OnInit {
 
-  generationCollection: object[];
+  generationCollection: GenerationCollection[];
 
   constructor(private pokeApiService: PokeApiService) {
   }
@@ -19,10 +21,31 @@ export class GenerationsComponent implements OnInit {
     );
   }
 
-  generationNameToUpperCase(generationString: string): string {
-    const stringWithoutNumber = generationString.substring(0, 10);
+  formatGenerationName(generationName: string): string {
+    const stringWithoutNumber = generationName.substring(0, 10);
     const uppercaseName = stringWithoutNumber.charAt(0).toUpperCase() + stringWithoutNumber.slice(1);
-    const romanNumeralsUppercase = generationString.substring(11).toUpperCase();
+    const romanNumeralsUppercase = generationName.substring(11).toUpperCase();
     return `${uppercaseName} ${romanNumeralsUppercase}`;
+  }
+
+  getGamesForGeneration(generationName: string): string {
+    switch (generationName) {
+      case 'generation-i':
+        return games4Generation['generation-i'].valueOf();
+      case 'generation-ii':
+        return games4Generation['generation-ii'].valueOf();
+      case 'generation-iii':
+        return games4Generation['generation-iii'].valueOf();
+      case 'generation-iv':
+        return games4Generation['generation-iv'].valueOf();
+      case 'generation-v':
+        return games4Generation['generation-v'].valueOf();
+      case 'generation-vi':
+        return games4Generation['generation-vi'].valueOf();
+      case 'generation-vii':
+        return games4Generation['generation-vii'].valueOf();
+      case 'generation-viii':
+        return games4Generation['generation-viii'].valueOf();
+    }
   }
 }
