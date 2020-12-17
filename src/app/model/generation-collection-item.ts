b917f59502e197e0234f20ee51b16979c2887d06
DatSwissGuy <kxosz9@outlook.com>
2020-12-17 23:15:49 +0100

    Rename types to pokemon-types.ts including classes and references.

--- src/app/model/generation-collection-item.ts
+++ src/app/model/generation-collection-item.ts
@@ -2,7 +2,7 @@ export interface GenerationCollectionItem<T1, T2, T3> {
   id: number;
   main_region: object;
   name: string;
-  pokemon_species: T1[];
-  types: T2[];
-  version_groups: T3[];
+  pokemon_species: PokemonSpecies[];
+  types: PokemonTypes[];
+  version_groups: VersionGroups[];
 }
