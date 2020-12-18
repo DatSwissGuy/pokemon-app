dba3cc9d2f1199f2911cfa65f23b5d54f13f584d
DatSwissGuy <kxosz9@outlook.com>
2020-12-18 23:37:14 +0100

    Add cards to generation item / pokemon list. Remove underline from generation list.

--- src/app/components/generation/generation.component.ts
+++ src/app/components/generation/generation.component.ts
@@ -19,4 +19,21 @@ export class GenerationComponent implements OnInit {
       pokeApiResponse => this.generation = pokeApiResponse
     );
   }
+
+  nameToUpperCase(pokemonName: string): string {
+    return pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+  }
+
+  formatVersionGroups(versionGroups: GenerationCollectionItem): string {
+    let gamesList = '';
+    versionGroups.version_groups.forEach(games => gamesList += `${games.name.replace('-', ' ')} `);
+    return gamesList;
+  }
+
+  getPokemonSvgUrl(pokemonUrl: string): string {
+    return pokemonUrl
+      .replace('https://pokeapi.co/api/v2/pokemon-species', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world')
+      .slice(0, -1)
+      .concat('.svg');
+  }
 }
