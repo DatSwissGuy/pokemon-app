d8aa98bc5464dd0dc3095a4ff9be97e685301cdb
DatSwissGuy <kxosz9@outlook.com>
2020-12-18 13:11:09 +0100

    Replace generics with types in  generation collection item model

--- src/app/model/generation-collection-item.ts
+++ src/app/model/generation-collection-item.ts
@@ -1,4 +1,8 @@
-export interface GenerationCollectionItem<T1, T2, T3> {
+import { PokemonSpecies } from './pokemon-species';
+import { PokemonTypes } from './pokemon-types';
+import { VersionGroups } from './version-groups';
+
+export interface GenerationCollectionItem {
   id: number;
   main_region: object;
   name: string;
