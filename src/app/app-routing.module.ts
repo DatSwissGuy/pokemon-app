477f0ab55e8ddc13a9c516379d0f3080a692f1b0
Christian M <51787895+DatSwissGuy@users.noreply.github.com>
2020-12-28 14:10:53 +0100

    Merge develop into master (#37)
    * Update dependencies (as well as Angular 11 migration) (#31)
    
    * Update angular core
    
    * Update angular cli
    
    * Update angular universal
    
    * Update the rest
    
    * Update dependencies 2 (#34)
    
    * Update Angular core
    
    * Update Angular material
    
    * Update depencies, npm audit
    
    * Remove package-lock from .gitignore
    
    * General code restyling (#33)
    
    * Edit editor config (IDEA): space between brackets for imports, double quotes
    
    * Remove [NBSP]
    
    * Rename types to pokemon-types.ts including classes and references.
    
    * Set typescript version between 4.0.0 and 4.1.0 in package.json
    
    * Replace generics with types in  generation collection item model
    
    * Restyle footer. Format code and remove unnecessary imports.
    
    * Use concat instead of + in getPokemonSvgUrl function.
    
    * Rename constants for better reading.
    
    * Clickable generation card (#35)
    
    * Initial components and logic. All clicks on generation cards lead to generation 1.
    
    * Add cards to generation item / pokemon list. Remove underline from generation list.
    
    * Add subtitles for card, add function for version_group formatting.
    
    * Added new subtitle for pokemon list from generation. Changed homescreen Pokemon to list 251 entries.
    
    * Change return type of formatVersionGroups function
    
    * Update dependencies via npm update / audit (#36)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
 import { GenerationsComponent } from './pages/generations/generations.component';
+import { GenerationComponent } from './components/generation/generation.component';
 
 const routes: Routes = [
   {path: '', component: PokemonComponent},
-  {path: 'generations', component: GenerationsComponent}
+  {path: 'generations', component: GenerationsComponent},
+  {path: 'generations/1', component: GenerationComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled',
     relativeLinkResolution: 'legacy'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/model/pokemon-types.ts
+++ src/app/model/pokemon-types.ts
@@ -1,4 +1,4 @@
-export interface Types {
+export interface PokemonTypes {
   name: string;
   url: string;
 }
