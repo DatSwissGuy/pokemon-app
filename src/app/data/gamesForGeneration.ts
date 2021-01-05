c161e007981d4ed9438f358393d2e2c874d7f617
DatSwissGuy <kxosz9@outlook.com>
2021-01-05 13:22:29 +0100

    Modify gamesForGeneration.ts to use numbers instead of a string.

--- src/app/data/gamesForGeneration.ts
+++ src/app/data/gamesForGeneration.ts
@@ -1,11 +1,11 @@
 export const gamesForGeneration =
   {
-    generationOne: 'Pokémon Red, Blue and Yellow',
-    generationTwo: 'Pokémon Silver, Gold and Crystal',
-    generationThree: 'Pokémon Ruby, Sapphire, FireRed, LeafGreen, Emerald',
-    generationFour: 'Pokémon Diamond, Pearl, Platinum, HeartGold and SoulSilver',
-    generationFive: 'Pokémon Black, White, Black 2 and White 2',
-    generationSix: 'Pokémon X, Y, Omega Ruby and Alpha Sapphire',
-    generationSeven: 'Pokémon Sun, Moon, Ultra Sun, Ultra Moon, Pokémon: Let\'s Go, Pickachu! and Pokémon Let\'s Go, Evee!',
-    generationEight: 'Pokémon Sword and Shield'
+    1: 'Pokémon Red, Blue and Yellow',
+    2: 'Pokémon Silver, Gold and Crystal',
+    3: 'Pokémon Ruby, Sapphire, FireRed, LeafGreen, Emerald',
+    4: 'Pokémon Diamond, Pearl, Platinum, HeartGold and SoulSilver',
+    5: 'Pokémon Black, White, Black 2 and White 2',
+    6: 'Pokémon X, Y, Omega Ruby and Alpha Sapphire',
+    7: 'Pokémon Sun, Moon, Ultra Sun, Ultra Moon, Pokémon: Let\'s Go, Pickachu! and Pokémon Let\'s Go, Evee!',
+    8: 'Pokémon Sword and Shield'
   };
