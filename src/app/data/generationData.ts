abfee62b55a7ef22e6bfc3cf88b4809c60658caf
DatSwissGuy <kxosz9@outlook.com>
2021-01-18 16:47:20 +0100

    Replace gamesForGeneration and pokemonNumberForGeneration with new data set (generationData.ts). Update switch case in generations.component.ts.

--- src/app/data/generationData.ts
+++ src/app/data/generationData.ts
@@ -1,14 +1,50 @@
 export const generationData = {
   1: {
-     'firstPokemon': 1,
-     'lastPokemon': 151,
-     'games': ['Pokémon Red', 'Pokémon Blue', 'Pokémon Yellow'],
-     'system': 'Game Boy'
+     firstPokemon: 1,
+     lastPokemon: 151,
+     games: ['Red', 'Blue', 'Yellow'],
+     platform: 'Game Boy'
   },
   2: {
-    'firstPokemon': 152,
-    'lastPokemon': 251,
-    'games': ['Pokémon Silver', 'Pokémon Gold', 'Pokémon Crystal'],
-    'system': 'Game Boy Color'
-  }  
-} 
+    firstPokemon: 152,
+    lastPokemon: 251,
+    games: ['Silver', 'Gold', 'Crystal'],
+    platform: 'Game Boy Color'
+  },
+  3: {
+    firstPokemon: 252,
+    lastPokemon: 386,
+    games: ['Ruby, Sapphire, FireRed, LeafGreen, Emerald'],
+    platform: 'Game Boy Advance'
+  },
+  4: {
+    firstPokemon: 384,
+    lastPokemon: 493,
+    games: ['Diamond, Pearl, Platinum, HeartGold, SoulSilver'],
+    platform: 'Game Boy Advance'
+  },
+  5: {
+    firstPokemon: 494,
+    lastPokemon: 649,
+    games: ['Black, White, Black 2, White 2'],
+    platform: 'Nintendo DS'
+  },
+  6: {
+    firstPokemon: 650,
+    lastPokemon: 721,
+    games: ['X, Y, Omega Ruby, Alpha Sapphire'],
+    platform: 'Nintendo 3DS'
+  },
+  7: {
+    firstPokemon: 722,
+    lastPokemon: 809,
+    games: ['Sun, Moon, Ultra Sun, Ultra Moon', 'Pokémon: Let\'s Go, Pickachu!, Pokémon Let\'s Go, Evee!'],
+    platform: 'Nintendo 3DS, Nintendo Switch'
+  },
+  8: {
+    firstPokemon: 810,
+    lastPokemon: 898,
+    games: ['Sword, Shield'],
+    platform: 'Nintendo Switch'
+  }
+};
