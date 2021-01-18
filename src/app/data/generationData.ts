47c1c5a8fe85f58de300c41f127b6d4f07b35aa8
DatSwissGuy <kxosz9@outlook.com>
2021-01-18 12:58:31 +0100

    New file for consolidated generation data

---
+++ src/app/data/generationData.ts
@@ -0,0 +1,14 @@
+export const generationData = {
+  1: {
+     'firstPokemon': 1,
+     'lastPokemon': 151,
+     'games': ['Pokémon Red', 'Pokémon Blue', 'Pokémon Yellow'],
+     'system': 'Game Boy'
+  },
+  2: {
+    'firstPokemon': 152,
+    'lastPokemon': 251,
+    'games': ['Pokémon Silver', 'Pokémon Gold', 'Pokémon Crystal'],
+    'system': 'Game Boy Color'
+  }  
+} 
