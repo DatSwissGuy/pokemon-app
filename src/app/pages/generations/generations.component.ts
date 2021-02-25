15b671e600fca39d909aef0f3cbe6cd40c6664de
Christian M <51787895+DatSwissGuy@users.noreply.github.com>
2021-02-25 16:07:29 +0100

    WIP: Enhanced generation data (#81)
    * Start with generation data improvements.
    
    * Restyle generation page. !Wrong branch!
    
    * Remove boxarts in generation list. Add pokemon sprites in its place (boxart pokemon).
    
    * Finalize styles.
    
    * Stop with enhancedGenerationData.ts. Add TODO and leave it be.

--- src/app/pages/generations/generations.component.ts
+++ src/app/pages/generations/generations.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PokeApiService } from '../../services/poke-api.service';
-import { GenerationCollection } from '../../model/collections/generation-collection';
+import { PokeApiService } from '../../services/poke-api.service'; import { GenerationCollection } from '../../model/collections/generation-collection';
 import { generationData } from '../../data/generationData';
+import { enhancedGenerationData } from '../../data/enhancedGenerationData';
 
 @Component({
   selector: 'app-generations',
@@ -26,6 +26,61 @@ export class GenerationsComponent implements OnInit {
     const uppercaseName = nameWithoutNumber.charAt(0).toUpperCase() + nameWithoutNumber.slice(1);
     const romanNumeralsUppercase = generationName.substring(11).toUpperCase();
     return `${uppercaseName} ${romanNumeralsUppercase}`;
+  }
+
+  returnBoxartForGeneration(generationName: string): string[] {
+    switch (generationName.substring(11)) {
+      case 'i':
+        return [
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png',
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'
+        ];
+      case 'ii': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/250.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/245.png'
+      ];
+      case 'iii': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/383.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/382.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/384.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
+      ];
+      case 'iv': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/483.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/484.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/487.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/250.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png',
+      ];
+      case 'v': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/643.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/644.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/646-black.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/646-white.png'
+      ];
+      case 'vi': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/716.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/717.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/383.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/382.png'
+      ];
+      case 'vii': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/791.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/792.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10155.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10156.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png'
+      ];
+      case 'viii': return [
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/888.png',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/889.png'
+
+      ];
+    }
   }
 
   // TODO find a more elegant solution...
